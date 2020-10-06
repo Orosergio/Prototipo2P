d3bdddaca9a45601c85b515ca3e57686ae119bbb
Orosergio <orosergioo@gmail.com>
2020-10-06 09:24:12 -0600

    Mantenimientos

---
+++ ParcialMVC/CapaControlador/Controlador.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaModelo;
+using System.Data;
+using System.Data.Odbc;
+namespace CapaControlador
+{
+    public class Controlador
+    {
+        Sentencias sn = new Sentencias();
+    }
+}
